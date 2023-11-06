27fe028dc279236545ff00ab3b28ac764d3654fb
Asma <test@test.com>
2023-11-06 10:51:31 -0500

    typecode

---
+++ src/rose/lily.java
@@ -0,0 +1,20 @@
+package rose;
+
+public class lily {
+
+	public static void main(String[] args) {
+	lily daisy = new lily ();
+	daisy.dalia();
+
+
+	}
+public void dalia () {
+	
+	
+	int a = 100;
+	int s = 200;
+	int c = a*s;
+	System.out.println(c);
+
+}
+}
